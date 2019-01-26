b43d9f61dab6564174f3853249ab8c271bebd629
yakkomajuri <38760734+yakkomajuri@users.noreply.github.com>
2019-01-26 17:47:24 +0000

    Update VaccineRegistration.sol

--- contracts/VaccineRegistration.sol
+++ contracts/VaccineRegistration.sol
@@ -1,39 +1,36 @@
-pragma solidity^0.4.21;
+pragma solidity ^ 0.4.21;
 
 contract VaccineRegistration {
 
-address owner;
-address secondary;
-address tertiary;
+    address owner;
+    address secondary;
+    address tertiary;
 
-constructor(address _secondary, address _tertiary) public {
-  owner = msg.sender;
-  secondary = _secondary;
-  tertiary = _tertiary;
-}
+    constructor(address _secondary, address _tertiary) public {
+        owner = msg.sender;
+        secondary = _secondary;
+        tertiary = _tertiary;
+    }
 
-modifier onlyOwners() {
-  require(msg.sender == owner ||
-    msg.sender == secondary ||
-    msg.sender == tertiary);
-    _;
-}
+    modifier onlyOwners() {
+        require(msg.sender == owner ||
+            msg.sender == secondary ||
+            msg.sender == tertiary);
+        _;
+    }
 
-string[] vaccines;
+    string[] vaccines;
 
-function registerData(string _data) public onlyOwners {
-  vaccines.push(_data);
-}
+    function registerData(string _data) public onlyOwners {
+        vaccines.push(_data);
+    }
 
-function getData(uint _index) public view returns(string _data) {
-  return vaccines[_index];
-}
+    function getData(uint _index) public view returns(string _data) {
+        return vaccines[_index];
+    }
 
-function getLength() public view returns(uint _length) {
-    return vaccines.length;
-}
+    function getLength() public view returns(uint _length) {
+        return vaccines.length;
+    }
 
-function deleteData(uint _index) public onlyOwners {
-    vaccines.length = _index;
-}
 }
